78deb21372f18812b089f160f7510b59ab625a5c
guilleavi <guillerminaavigliano@yahoo.com.ar>
2020-08-21 19:35:39 -0300

    cleaning app autogenerated code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,23 +9,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'movie-theater-coding-challenge'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('movie-theater-coding-challenge');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('movie-theater-coding-challenge app is running!');
-  });
 });
